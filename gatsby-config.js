24601ee119b981b2e2fcae27c259038b13ff8921
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2019-07-29 08:59:41 -0400

    minor updates - fav icon, update classes
    TO DO: dark theme, summer term reflections (now), add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@aditirajagopal`,
   },
   plugins: [
+    `gatsby-plugin-dark-mode`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
@@ -25,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/fav-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
