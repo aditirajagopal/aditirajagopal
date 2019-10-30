358fcf4f9f980aa4d0ce49aad1887676eac585a5
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2019-10-29 23:08:14 -0400

    fix deploy issue - take 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages/blog`,
-        name: 'blog',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         path: `${__dirname}/src/pages/favoritesLog`,
         name: 'favorites',
       },
