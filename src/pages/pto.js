eff6da2802ff1d3563520cf8869356d618c472e6
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2019-12-24 19:13:08 -0500

    fix

--- src/pages/pto.js
+++ src/pages/pto.js
@@ -11,7 +11,6 @@ const PTOPage = () => (
 				<h2 class="f3 mid-gray lh-title mt2">
 					<a href="https://www.bcg.com/careers/working-at-bcg/corporate-culture.aspx">Predictability, Teaming, and Open Communication (PTO)</a>
 				</h2>
-				<time class="f6 ttu tracked gray">Last Updated: Oct. 2019</time>
 				<p class="measure lh-copy">
 					<ol>
 						<li>Hours & Scheduling</li>
@@ -29,7 +28,8 @@ const PTOPage = () => (
 			</p>
 		</article>
 		<footer class="pv4 ph3 ph5-m ph6-l mid-gray">
-			<small class="mt3 tc">Inspired by <a href="https://amy.dev/?p=979">Amy Nguyen</a></small>
+			<time class="f6 ttu tracked gray"><small>Last Updated: Oct. 2019</small></time>
+			<small class="mt3">Inspired by <a href="https://amy.dev/?p=979">Amy Nguyen</a></small>
 		</footer>
   </Layout>
 )
